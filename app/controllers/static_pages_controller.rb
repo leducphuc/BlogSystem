938e639b2635f95156cef20efc83e1e98f4a3f01
ducphucle <leducphuc1994@gmail.com>
2016-05-20 18:02:31 +0700

    Static Pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def new
+  end
+
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'new' => 'static_pages#new'
+
+  get 'help' => 'static_pages#help'
+
+  get 'about' => 'static_pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
