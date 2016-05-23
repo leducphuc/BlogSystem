7639ba81896b18ff6ef6080ff9f780fd500df279
ducphucle <leducphuc1994@gmail.com>
2016-05-24 02:13:22 +0700

    complete user login logout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
   end
 
   def home
+    @entry = current_user.entries.build if logged_in?
+    @feed_items = current_user.feed.paginate(page:params[:page])
   end
 
   def help
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index,:following,:followers]
   def new
     @user = User.new
   end
@@ -16,7 +17,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    # @microposts = @user.microposts.paginate(page: params[:page])
+    @entry= @user.entries.paginate(page: params[:page])
   end
 
   def index
@@ -30,9 +31,17 @@ class UsersController < ApplicationController
   end
 
   def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page:params[:page])
+    render 'show_follow'
   end
 
   def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page:params[:page])
+    render 'show_follow'
   end
 
   private
